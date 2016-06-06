0ee766fc502fc7044a01e4b52743f8cd1bdcd57e
tezign-jason <gj@tezign.com>
2016-06-07 01:31:57 +0800

    sutdio part2

--- custom/template/studio-detail.php
+++ custom/template/studio-detail.php
@@ -23,11 +23,11 @@ get_header();
             <p class="content">我們将为每一位真心想来日本完成艺术梦想的孩子提供最专业的日本美术大学的个性化进学指导课 程。号称“东京五美”的武藏野美术大学、多摩美術大学、女子美術大学、東京造形大学、日本大学芸術学部，远至京都方面的美术院校都有CAplus的成员。丰富的成员资源也为我们提供个性化培训提供 了强有力的基础。我们没有后台也没有分红合作的机构，只是一群在日本学美术的年轻人聚集在一起，如果你要来留学，请先来CAplus工作室瞧瞧</p>
         </div>
         <div class="part-2 col-lg-12 ca-block">
-            <img src="<?=APP_URL.'/custom/images/studio-item-01.png';?>" alt="">
+            <img src="<?=APP_URL.'/custom/images/studio-item-011.png';?>" alt="">
         </div>
         <div class="part-3 col-lg-12 ca-block">
             <div class="col-lg-2 left">
-                <img src="<?=APP_URL.'/custom/images/studio-item-02.png';?>" alt="">
+                <img src="<?=APP_URL.'/custom/images/studio-item-012.png';?>" alt="">
             </div>
             <div class="col-lg-8 col-lg-offset-2 right">
                 <p>
@@ -47,7 +47,116 @@ get_header();
         </div>
     </div>
     <div class="col-lg-10 col-lg-offset-1 tab-content item-2">
-        <p>222222</p>
+        <div class="part-1 col-lg-12 ca-block">
+            <p class="title">漂亮話不多說<br>找老師還是靠譜得好</p>
+            <p class="content">我們将为每一位真心想来日本完成艺术梦想的孩子提供最专业的日本美术大学的个性化进学指导课 程。号称“东京五美”的武藏野美术大学、多摩美術大学、女子美術大学、東京造形大学、日本大学芸術 学部,远至京都方面的美术院校都有 CAplus 的成员。丰富的成员资源也为我们提供个性化培训提供 了强有力的基础。我们没有后台也没有分红合作的机构，只是一群在日本学美术的年轻人聚集在一起，如果你要来留学，请先來 CAplus 工作室瞧瞧。</p>
+        </div>
+        <div class="part-2 col-lg-12 ca-block">
+            <div class="col-lg-12 ca-block">
+                <div class="col-lg-6 brief">
+                    <div class="head ca-block">
+                        <div class="pic">
+                            <img src="<?=APP_URL.'/custom/images/studio-item-021.png';?>" alt="">
+                        </div>
+                        <div class="title">
+                            <div class="name">陳 芃</div>
+                            <div class="line"></div>
+                            <div class="study">
+                                <span>武藏野美術大学</span><br>
+                                造形学部<br>
+                                基礎デザイン学科
+                            </div>
+                        </div>
+                    </div>
+                    <div class="content">
+                        CAplus 對於我來說不僅僅是壹個夢想的開始，<br>
+                        並且是我向著這個所謂的現實世界的一種抵抗，我一向認為年輕人就該有自己的夢想並且去實現它。<br>
+                        特別對於學習藝術的我們。創意、激情這些美好的詞匯全部源於年輕這個詞匯，這個美好的時間卻只占了我們人生的1/7，為什麽我們不能好好利用這個時間來做一些能改變的事情呢？<br>
+                        幾年前我來到東京的時候感覺到了這個遠東國家的強大的文化吸引力，文化是在撞擊和融合下產生的產物，沒有新的文化的生成世界也就不會變得更有趣，我們作為留學生這個異文化體來到日本，並且在這個國家的學習過程中產生了產生奇妙的化學效應，我將之宣傳出來並且將所有和我有一樣想法的人聚集在一起，形成了 CAplus 這個團體。
+                    </div>
+                </div>
+                <div class="col-lg-6 brief">
+                    <div class="head ca-block">
+                        <div class="pic">
+                            <img src="<?=APP_URL.'/custom/images/studio-item-022.png';?>" alt="">
+                        </div>
+                        <div class="title">
+                            <div class="name">徐澤渝 </div>
+                            <div class="line"></div>
+                            <div class="study">
+                                <span>武藏野美術大学</span><br>
+                                造形学部<br>
+                                基礎デザイン学科
+                            </div>
+                        </div>
+                    </div>
+                    <div class="content">
+                        夢想 現實 一步之遙<br>
+                        那麽這一步就是 CAplus<br>
+                        在我更年輕一點的高中時代<br>
+                        常因為把夢想理解的遙不可及而讓自己生活的非常消極<br>
+                        直到我決定來到日本 邁向了新生活 邁向了走上實踐的道路<br>
+                        如果夢想只是停留在想 那麽永遠都是個夢<br>
+                        現在的我非常感謝當年自己的付出 在往返三小時的電車上補覺 在大家都回<br>
+                        家休息的時候我選擇去畫室 在大家都質疑我的時候堅持了下來<br>
+                        我所遇見的 我將毫無保留的分享<br>
+                        有著同樣夢想的我們成為了 CAplus 有著不同經歷的我們成為了不同的絢爛 。
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12 ca-blocl">
+                <div class="col-lg-6 brief">
+                    <div class="head ca-block">
+                        <div class="pic">
+                            <img src="<?=APP_URL.'/custom/images/studio-item-021.png';?>" alt="">
+                        </div>
+                        <div class="title">
+                            <div class="name">陳 芃</div>
+                            <div class="line"></div>
+                            <div class="study">
+                                <span>武藏野美術大学</span><br>
+                                造形学部<br>
+                                基礎デザイン学科
+                            </div>
+                        </div>
+                    </div>
+                    <div class="content">
+                        CAplus 對於我來說不僅僅是壹個夢想的開始，<br>
+                        並且是我向著這個所謂的現實世界的一種抵抗，我一向認為年輕人就該有自己的夢想並且去實現它。<br>
+                        特別對於學習藝術的我們。創意、激情這些美好的詞匯全部源於年輕這個詞匯，這個美好的時間卻只占了我們人生的1/7，為什麽我們不能好好利用這個時間來做一些能改變的事情呢？<br>
+                        幾年前我來到東京的時候感覺到了這個遠東國家的強大的文化吸引力，文化是在撞擊和融合下產生的產物，沒有新的文化的生成世界也就不會變得更有趣，我們作為留學生這個異文化體來到日本，並且在這個國家的學習過程中產生了產生奇妙的化學效應，我將之宣傳出來並且將所有和我有一樣想法的人聚集在一起，形成了 CAplus 這個團體。
+                    </div>
+                </div>
+                <div class="col-lg-6 brief">
+                    <div class="head ca-block">
+                        <div class="pic">
+                            <img src="<?=APP_URL.'/custom/images/studio-item-022.png';?>" alt="">
+                        </div>
+                        <div class="title">
+                            <div class="name">徐澤渝 </div>
+                            <div class="line"></div>
+                            <div class="study">
+                                <span>武藏野美術大学</span><br>
+                                造形学部<br>
+                                基礎デザイン学科
+                            </div>
+                        </div>
+                    </div>
+                    <div class="content">
+                        夢想 現實 一步之遙<br>
+                        那麽這一步就是 CAplus<br>
+                        在我更年輕一點的高中時代<br>
+                        常因為把夢想理解的遙不可及而讓自己生活的非常消極<br>
+                        直到我決定來到日本 邁向了新生活 邁向了走上實踐的道路<br>
+                        如果夢想只是停留在想 那麽永遠都是個夢<br>
+                        現在的我非常感謝當年自己的付出 在往返三小時的電車上補覺 在大家都回<br>
+                        家休息的時候我選擇去畫室 在大家都質疑我的時候堅持了下來<br>
+                        我所遇見的 我將毫無保留的分享<br>
+                        有著同樣夢想的我們成為了 CAplus 有著不同經歷的我們成為了不同的絢爛 。
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
