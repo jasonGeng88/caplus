e67c103c28a864510fe2b48af463e888adeb1a02
tezign-jason <gj@tezign.com>
2016-07-24 16:51:46 +0800

    studio && studio detail css

--- custom/template/studio-detail.php
+++ custom/template/studio-detail.php
@@ -10,8 +10,8 @@ foreach ($urls as &$item) {
 }
 ?>
 
-<div class="col-md-12 studio-detail ca-block">
-    <div class="col-md-12 tab-title ca-block">
+<div class="col-md-12 col-xs-12 studio-detail ca-block">
+    <div class="col-md-12 tab-title ca-block ca-pc">
         <div class="col-md-10 col-md-offset-1">
             <div class="name">
                 <a href="javascript:toggleTab(1)">環境&地點</a>
@@ -27,19 +27,34 @@ foreach ($urls as &$item) {
             </div>
         </div>
     </div>
-    <div class="col-md-10 col-md-offset-1 tab-content item-1">
-        <div class="part-1 col-md-12 ca-block">
+
+    <div class="col-xs-12 tab-title ca-block ca-mobile">
+        <div class="name col-xs-3">
+            <a href="javascript:toggleTab(1)"><p>環境&地點</p></a>
+        </div>
+        <div class="name col-xs-3">
+            <a href="javascript:toggleTab(2)"><p>講師團隊</p></a>
+        </div>
+        <div class="name col-xs-3">
+            <a href="javascript:toggleTab(3)"><p>课程安排</p></a>
+        </div>
+        <div class="name col-xs-3">
+            <a href="javascript:toggleTab(4)"><p>無料體驗</p></a>
+        </div>
+    </div>
+    <div class="col-md-10 col-md-offset-1 col-xs-12 tab-content item-1">
+        <div class="part-1 col-md-12 col-xs-12 ca-block">
             <p class="title ca-line">SOHO 設計事務所兼畫室<br>美大零距離體驗 coffee & music 和聚餐 一個不能少</p>
             <p class="content ca-line">我們将为每一位真心想来日本完成艺术梦想的孩子提供最专业的日本美术大学的个性化进学指导课 程。号称“东京五美”的武藏野美术大学、多摩美術大学、女子美術大学、東京造形大学、日本大学芸術学部，远至京都方面的美术院校都有CAplus的成员。丰富的成员资源也为我们提供个性化培训提供 了强有力的基础。我们没有后台也没有分红合作的机构，只是一群在日本学美术的年轻人聚集在一起，如果你要来留学，请先来CAplus工作室瞧瞧</p>
         </div>
-        <div class="part-2 col-md-12 ca-block">
+        <div class="part-2 col-md-12 col-xs-12 ca-block">
             <img src="<?=APP_URL.'/custom/images/studio-item-011.png';?>" alt="">
         </div>
-        <div class="part-3 col-md-12 ca-block">
-            <div class="col-md-2 left">
+        <div class="part-3 col-md-12 col-xs-12 ca-block">
+            <div class="col-md-2 col-xs-12 left">
                 <img src="<?=APP_URL.'/custom/images/studio-item-012.png';?>" alt="">
             </div>
-            <div class="col-md-8 col-md-offset-2 right ca-line">
+            <div class="col-md-8 col-md-offset-2 col-xs-12 right ca-line">
                 <p>
                     MONA:080-3710-6088<br>
                     地下鉄東西線高田馬場駅早稲田口 4分<br>
@@ -61,7 +76,7 @@ foreach ($urls as &$item) {
             <p class="title">漂亮話不多說<br>找老師還是靠譜得好</p>
             <p class="content ca-line">我們将为每一位真心想来日本完成艺术梦想的孩子提供最专业的日本美术大学的个性化进学指导课 程。号称“东京五美”的武藏野美术大学、多摩美術大学、女子美術大学、東京造形大学、日本大学芸術 学部,远至京都方面的美术院校都有 CAplus 的成员。丰富的成员资源也为我们提供个性化培训提供 了强有力的基础。我们没有后台也没有分红合作的机构，只是一群在日本学美术的年轻人聚集在一起，如果你要来留学，请先來 CAplus 工作室瞧瞧。</p>
         </div>
-        <div class="part-2 col-md-12 ca-block">
+        <div class="part-2 col-md-12 ca-block ca-pc">
             <div class="col-md-12 ca-block">
                 <div class="col-md-6 brief">
                     <div class="head ca-block">
@@ -167,6 +182,55 @@ foreach ($urls as &$item) {
                 </div>
             </div>
         </div>
+
+        <div class="part-2 col-xs-12 ca-block ca-mobile">
+            <div class="col-xs-12 brief">
+                <div class="head col-xs-12 ca-block">
+                    <img src="<?=APP_URL.'/custom/images/studio-item-021.png';?>" alt="">
+                </div>
+                <div class="title col-xs-12">
+                    <div class="name">陳 芃</div>
+                    <div class="line"></div>
+                    <div class="study">
+                        <span>武藏野美術大学</span><br>
+                        造形学部<br>
+                        基礎デザイン学科
+                    </div>
+                </div>
+                <div class="content col-xs-12">
+                    CAplus 對於我來說不僅僅是壹個夢想的開始，<br>
+                    並且是我向著這個所謂的現實世界的一種抵抗，我一向認為年輕人就該有自己的夢想並且去實現它。<br>
+                    特別對於學習藝術的我們。創意、激情這些美好的詞匯全部源於年輕這個詞匯，這個美好的時間卻只占了我們人生的1/7，為什麽我們不能好好利用這個時間來做一些能改變的事情呢？<br>
+                    幾年前我來到東京的時候感覺到了這個遠東國家的強大的文化吸引力，文化是在撞擊和融合下產生的產物，沒有新的文化的生成世界也就不會變得更有趣，我們作為留學生這個異文化體來到日本，並且在這個國家的學習過程中產生了產生奇妙的化學效應，我將之宣傳出來並且將所有和我有一樣想法的人聚集在一起，形成了 CAplus 這個團體。
+                </div>
+            </div>
+            <div class="col-xs-12 brief">
+                <div class="head col-xs-12 ca-block">
+                    <img src="<?=APP_URL.'/custom/images/studio-item-022.png';?>" alt="">
+                </div>
+                <div class="title col-xs-12">
+                    <div class="name">徐澤渝 </div>
+                    <div class="line"></div>
+                    <div class="study">
+                        <span>武藏野美術大学</span><br>
+                        造形学部<br>
+                        基礎デザイン学科
+                    </div>
+                </div>
+                <div class="content col-xs-12">
+                    夢想 現實 一步之遙<br>
+                    那麽這一步就是 CAplus<br>
+                    在我更年輕一點的高中時代<br>
+                    常因為把夢想理解的遙不可及而讓自己生活的非常消極<br>
+                    直到我決定來到日本 邁向了新生活 邁向了走上實踐的道路<br>
+                    如果夢想只是停留在想 那麽永遠都是個夢<br>
+                    現在的我非常感謝當年自己的付出 在往返三小時的電車上補覺 在大家都回<br>
+                    家休息的時候我選擇去畫室 在大家都質疑我的時候堅持了下來<br>
+                    我所遇見的 我將毫無保留的分享<br>
+                    有著同樣夢想的我們成為了 CAplus 有著不同經歷的我們成為了不同的絢爛 。
+                </div>
+            </div>
+        </div>
     </div>
     <div class="col-md-10 col-md-offset-1 tab-content item-3">
         <div class="part-1 col-md-12 ca-block">
@@ -211,21 +275,21 @@ foreach ($urls as &$item) {
                 <p class="content">我們将为每一位真心想来日本完成艺术梦想的孩子提供最专业的日本美术大学的个性化进学指导课程。号称“东京五美”的武藏野美术大学、多摩美術大学、女子美術大学、東京造形大学、日本大学芸術学部，远至京都方面的美术院校都有CAplus的成员。丰富的成员资源也为我们提供个性化培训提供了强有力的基础。我们没有后台也没有分红合作的机构，只是一群在日本学美术的年轻人聚集在一起如果你要来留学，请先来CAplus工作室瞧瞧。</p>
             </div>
         </div>
-        <div class="part-2 col-md-12 ca-block">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="input-group">
+        <div class="part-2 col-md-12 col-xs-12 ca-block">
+            <div class="col-md-10 col-md-offset-1 col-xs-12 email-info">
+                <div class="input-group col-xs-12">
                     <input type="text" class="email_name" name="email_name" placeholder="名字">
                 </div>
-                <div class="input-group">
+                <div class="input-group col-xs-12">
                     <input type="text" class="email_phone" name="email_phone" placeholder="聯係電話">
                 </div>
-                <div class="input-group">
+                <div class="input-group col-xs-12">
                     <input type="text" class="email_address" name="email_address" placeholder="郵件地址">
                 </div>
-                <div class="input-group">
+                <div class="input-group col-xs-12 email-content-group">
                     <textarea class="email_content" name="email_content" id="" cols="30" rows="10" placeholder="親愛的 CAplus 團隊："></textarea>
                 </div>
-                <div class="input-group">
+                <div class="input-group col-xs-12">
                     <button onclick="email()" class="btn-email">送信</button>
                 </div>
                 <div class="contact-info ca-block">
@@ -244,8 +308,13 @@ foreach ($urls as &$item) {
 <script>
     toggleTab(1);
     function toggleTab(index){
-        jQuery(".tab-title .name").removeClass("active");
-        jQuery(".tab-title .name").eq(index-1).addClass("active");
+        //pc
+        jQuery(".tab-title.ca-pc .name").removeClass("active");
+        jQuery(".tab-title.ca-pc .name").eq(index-1).addClass("active");
+        //mobile
+        jQuery(".tab-title.ca-mobile .name").removeClass("active");
+        jQuery(".tab-title.ca-mobile .name").eq(index-1).addClass("active");
+
         jQuery(".tab-content").css("display", "none");
         jQuery(".tab-content.item-" + index).css("display", "block");
     }
--- custom/template/studio.php
+++ custom/template/studio.php
@@ -3,7 +3,7 @@ get_header();
 ?>
 
 <div class="col-md-12 studio ca-block">
-    <div class="col-md-12 studio-entry ca-block">
+    <div class="col-md-12 studio-entry ca-block ca-pc">
         <div class="col-md-5 col-md-offset-1 pic">
             <button onclick="location.href='<?=STUDIO_DETAIL_URL;?>'">美大入試指導</button>
             <img src="<?=APP_URL.'/custom/images/studio_bg.png';?>" alt="">
@@ -13,7 +13,26 @@ get_header();
             <p>我們将为每一位真心想来日本完成艺术梦想的孩子 提供最专业的日本美术大学的个性化进学指导课程。 号称“东京五美”的武藏野美术大学、多摩美術大学、 女子美術大学、東京造形大学、日本大学芸術学部,远 至京都方面的美术院校都有CAplus的成员。丰富的 成员资源也为我们提供个性化培训提供了强有力的 基础。我们没有后台也没有分红合作的机构,只是一 群在日本学美术的年轻人聚集在一起,如果你要来留 学,请先来CAplus工作室瞧瞧</p>
         </div>
     </div>
-    <div class="col-md-12 studio-history">
+    <div class="col-xs-12 studio-entry ca-block ca-mobile">
+        <div class="col-xs-7 pic">
+            <img src="<?=APP_URL.'/custom/images/studio_bg.png';?>" alt="">
+        </div>
+        <div class="col-xs-5 content">
+            <p>我們将为每一位真心想来日本完成艺术梦想的孩子 提供最专业的日本美术大学的个性化进学指导课程。 号称“东京五美”的武藏野美术大学、多摩美術大学、 女子美術大学、東京造形大学、日本大学芸術学部,远 至京都方面的美术院校都有CAplus的成员。丰富的 成员资源也为我们提供个性化培训提供了强有力的 基础。我们没有后台也没有分红合作的机构,只是一 群在日本学美术的年轻人聚集在一起,如果你要来留 学,请先来CAplus工作室瞧瞧</p>
+        </div>
+    </div>
+    <div class="col-xs-12 entry-btn ca-mobile">
+        <a href="<?=STUDIO_DETAIL_URL;?>">
+            <p>美大入試指導<span class="icon-continue"></span></p>
+        </a>
+    </div>
+    <div class="col-xs-12 entry-btn ca-mobile">
+        <a href="">
+            <p>設計工作室<span class="icon-continue"></span></p>
+        </a>
+    </div>
+
+    <div class="col-md-12 studio-history ca-pc">
         <ul class="timeline">
             <li class="timeline-event">
                 <label class="timeline-event-icon"></label>
@@ -110,4 +129,115 @@ get_header();
 
         </ul>
     </div>
+
+    <div class="col-xs-12 studio-history ca-mobile">
+        <div class="col-xs-12 history-pic">
+            <img src='<?=of_get_option('logo', true);?>'>
+        </div>
+        <ul class="timeline">
+            <li class="timeline-event">
+                <label class="timeline-event-icon"></label>
+                <div class="timeline-event-copy">
+                    <p class="timeline-event-thumbnail">二〇一四年</p>
+                    <div class="point-item col-xs-12">
+                        <div class="point-name col-xs-3">31.Dec</div>
+                        <div class="point-content col-xs-9">
+                            <p>北海道，屬於留學生自己的設計品牌想法產生</p>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li class="timeline-event">
+                <label class="timeline-event-icon"></label>
+                <div class="timeline-event-copy">
+                    <p class="timeline-event-thumbnail">二〇一五年</p>
+                    <div class="point-item col-xs-12">
+                        <div class="point-name col-xs-3">Feb</div>
+                        <div class="point-content col-xs-9">
+                            <p>在武漢開始品牌soga的創立工作</p>
+                        </div>
+                    </div>
+                    <div class="point-item col-xs-12">
+                        <div class="point-name col-xs-3">Mar</div>
+                        <div class="point-content col-xs-9">
+                            <p>回到東京建立第一版官方網站，改名CA+</p>
+                            <p>开通官方微博，微信平台</p>
+                        </div>
+                    </div>
+                    <div class="point-item col-xs-12">
+                        <div class="point-name col-xs-3">Apr</div>
+                        <div class="point-content col-xs-9">
+                            <p>發布第一篇微博</p>
+                            <p>微信平台發布第一篇文章「你,不是一個人」</p>
+                        </div>
+                    </div>
+                    <div class="point-item col-xs-12">
+                        <div class="point-name col-xs-3">May</div>
+                        <div class="point-content col-xs-9">
+                            <p>拿到第一筆天使投資</p>
+                            <p>日本美大生紀錄片企劃開始</p>
+                            <p>和日本程序員合作,網站改版開始</p>
+                            <p>紀錄片第一彈「藤原芽衣vol1」推出,網站改版開始</p>
+                        </div>
+                    </div>
+                    <div class="point-item col-xs-12">
+                        <div class="point-name col-xs-3">Jun</div>
+                        <div class="point-content col-xs-9">
+                            <p>CAplus東京展示企劃開始</p>
+                            <p>出差北海道!拍攝紀錄片第二彈 坂下舞 vol 2</p>
+                            <p>展示會場決定</p>
+                        </div>
+                    </div>
+                    <div class="point-item col-xs-12">
+                        <div class="point-name col-xs-3">Sep</div>
+                        <div class="point-content col-xs-9">
+                            <p>CAplus 展示宣傳片開始拍攝</p>
+                        </div>
+                    </div>
+                    <div class="point-item col-xs-12">
+                        <div class="point-name col-xs-3">Oct</div>
+                        <div class="point-content col-xs-9">
+                            <p>贊助武藏野美鬼屋企劃，企劃武藏野美華人展</p>
+                        </div>
+                    </div>
+                    <div class="point-item col-xs-12">
+                        <div class="point-name col-xs-3">Dec</div>
+                        <div class="point-content col-xs-9">
+                            <p>微信平台和微博平台粉絲達到8000+</p>
+                        </div>
+                    </div>
+
+                </div>
+            </li>
+            <li class="timeline-event">
+                <label class="timeline-event-icon"></label>
+                <div class="timeline-event-copy">
+                    <p class="timeline-event-thumbnail">二〇一六年</p>
+                    <div class="point-item col-xs-12">
+                        <div class="point-name col-xs-3">Mar</div>
+                        <div class="point-content col-xs-9">
+                            <p>CAplus 第一次展示「Y-generation原宿」開始</p>
+                        </div>
+                    </div>
+                    <div class="point-item col-xs-12">
+                        <div class="point-name col-xs-3">Apr</div>
+                        <div class="point-content col-xs-9">
+                            <p>CAplus 東京工作室實體化開始</p>
+                        </div>
+                    </div>
+                </div>
+            </li>
+
+        </ul>
+    </div>
 </div>
+
+<script>
+    function adjustMobileEntry(){
+        var pic = jQuery(".studio-entry.ca-mobile .pic");
+        var content = jQuery(".studio-entry.ca-mobile .content");
+        pic.css("margin-top", (content.height() - pic.height())/2 + "px");
+    }
+    adjustMobileEntry();
+
+</script>
