9fcaf91aa78f88b4cdbd41727b0542aa462380a6
tezign-jason <gj@tezign.com>
2016-08-28 21:12:53 +0800

    add sql

--- todo.sql
+++ todo.sql
@@ -1,3 +1,7 @@
 update wp_posts set guid=REPLACE (guid,'www.localca.com','45.63.121.127');
 
 update wp_posts set post_content=REPLACE (post_content,'www.localca.com','45.63.121.127');
+
+update wp_options set option_value=REPLACE (option_value,'45.63.121.127','45.79.105.233');
+
+update wp_postmeta set meta_value=REPLACE (meta_value,'45.63.121.127','45.79.105.233');
