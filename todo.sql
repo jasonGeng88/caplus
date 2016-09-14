178d1925b631682db47248ef3b37b545232ca985
tezign-jason <gj@tezign.com>
2016-09-14 12:29:15 +0800

    change domain

--- todo.sql
+++ todo.sql
@@ -1,7 +1,9 @@
-update wp_posts set guid=REPLACE (guid,'www.localca.com','45.63.121.127');
+set @old_url='45.79.105.233';
+set @new_url='www.caplus.tokyo';
+update wp_posts set guid=REPLACE (guid, @old_url,@new_url);
 
-update wp_posts set post_content=REPLACE (post_content,'www.localca.com','45.63.121.127');
+update wp_posts set post_content=REPLACE (post_content, @old_url,@new_url);
 
-update wp_options set option_value=REPLACE (option_value,'45.63.121.127','45.79.105.233');
+update wp_options set option_value=REPLACE (option_value, @old_url,@new_url);
 
-update wp_postmeta set meta_value=REPLACE (meta_value,'45.63.121.127','45.79.105.233');
+update wp_postmeta set meta_value=REPLACE (meta_value, @old_url,@new_url);
